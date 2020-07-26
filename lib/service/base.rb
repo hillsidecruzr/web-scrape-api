c786cfad56f00b67c3f3423cd9b577f85c74a4b7
Trevor Broaddus <trevor.broaddus@daveramsey.com>
2020-07-26 08:28:58 -0500

    WIP working on transforming web scrape data into database models

--- lib/service/base.rb
+++ lib/service/base.rb
@@ -3,20 +3,26 @@ require "json"
 module Service
   class Base
     DATA_DIR = Rails.root.join("storage/data").freeze
-    FORMATS = [:json]
+    FORMATS = [:json, :database]
     EXTRACTORS = {}
 
-    attr_reader :url, :extractors
+    attr_reader :url, :extractors, :extractions
 
     def initialize(url:, extractors:)
       @url = url
       @extractors = extractors
+      @extractions = {}
     end
 
     def flush(format: :json, data:)
       return unless FORMATS.include? format
 
-      flush_json(data: data) if format == :json
+      case format
+      when :json
+        flush_json(data: data)
+      when :database
+        flush_database(data: data)
+      end
     end
 
     def filename_by_url
@@ -27,10 +33,34 @@ module Service
 
   private
 
+    def extract
+      extractors.each do |extractor_name, opts|
+        next unless valid_extractor?(extractor: extractor_name)
+
+        opts ||= {}
+
+        extractions[extractor_name] = EXTRACTORS[extractor_name].extract(document: document, **opts)
+      end
+    end
+
+    def valid_extractor?(extractor:)
+      EXTRACTORS.include? extractor
+    end
+
     def flush_json(data:)
       File.open("#{DATA_DIR}/#{filename_by_url}.json", "w") do |f|
         f.puts data.to_json
       end
+    end
+
+    def flush_database(data:)
+      Scrape.create({
+        name: url,
+        url: url,
+        site_name: "",
+        images: extractions[:images],
+        links: extractions[:links]
+      })
     end
   end
 end
--- lib/service/extractors/links.rb
+++ lib/service/extractors/links.rb
@@ -6,7 +6,27 @@ module Service
           return unless document.present?
 
           query = replace ? "#{context}" : "#{context}//a"
-          document.xpath(query).map { |tag| { href: tag[:href], text: tag[:text] } }
+          document.xpath(query).map { |tag| linkify tag }
+        end
+
+        def linkify(tag)
+          uri = URI.parse tag[:href]
+
+          Link.new({
+            url: uri.absolute? ? tag[:href] : fully_qualified_url tag[:href],
+            external: uri.absolute?,
+            occurrences: 1, # @todo: refactor to query the collection to find others
+          })
+
+          # conditionally add multimedia
+        end
+
+        def fully_qualified_url(relative_url)
+          result = document.xpath("//link[@rel='canonical']")
+
+          uri = URI.parse result.first[:href]
+
+          "#{uri.scheme}://#{uri.hostname}/#{relative_url}"
         end
       end
     end
