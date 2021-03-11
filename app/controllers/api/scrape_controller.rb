da2bad143e938097890f641f1ebd12831216cfc6
Trevor Broaddus <trevor.broaddus@daveramsey.com>
2021-03-10 20:39:06 -0600

    Add documentation and notes for upcoming interview

--- app/controllers/api/scrape_controller.rb
+++ app/controllers/api/scrape_controller.rb
@@ -1,8 +1,10 @@
 module Api
   class ScrapeController < ApplicationController
     def get
+      # @todo: Actually do this in the background
       WebScrapeJob.perform_now(url: site_url_param)
 
+      # @todo: Provide up-to-date execution status
       render json: {status: :executing}
     end
 
--- lib/service/concerns/fetchable.rb
+++ lib/service/concerns/fetchable.rb
@@ -4,10 +4,16 @@ module Service
       extend ActiveSupport::Concern
 
       class_methods do
+        # Creates a Proc using the method extractor. The `:fetch` method must be an instance method for this methodology
+        # to work.
+        #
+        # @see Object#method
         def fetchable
+          # @todo: Could refactor this (I think) to just `method(:fetch).to_proc`
           Proc.new(&method(:fetch))
         end
 
+        # Executes a simple GET request to the provided url.
         def fetch(stringify = true, url)
           response = Faraday.get url
 
@@ -20,10 +26,19 @@ module Service
       included do
         self.attr_reader :markup
 
+
+        # Proxy to the class method
+        #
+        # @note This was added to make a more fluid interface in the {Scraper} class so that we don't have to do a
+        # `self.class.fetchable`
+        #
+        # @see Scraper#exec
         def fetchable
           self.class.fetchable
         end
 
+        # Proxies to the class method, but is necessary so that {#fetchable} can use the {Object#method} feature to build
+        # a Proc object
         def fetch(stringify = true, url)
           @markup = self.class.fetch(stringify, url)
         end
